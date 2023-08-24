e779f1bb3c19859bc9b7d8eaae7f74a6432ffea6
leguilln <nicolas.leguillarme@univ-grenoble-alpes.fr>
2023-08-24 13:33:33 +0200

    Comment

--- src/ontology/template.sh
+++ src/ontology/template.sh
@@ -6,9 +6,3 @@ robot template \
   --merge-before \
   --output sfwo-edit-test.owl
 
-# robot template --template general_equivalence.csv  \
-#   --input sfwo-edit.owl \
-#   --prefix "SFWO: http://purl.org/sfwo/SFWO_" \
-#   --merge-before \
-#   --output sfwo-edit.owl
-
